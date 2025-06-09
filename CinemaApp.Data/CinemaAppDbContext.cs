2bd4a4229b498ebcc0414921dc709f77ef9acbb3
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-06-09 20:14:53 +0300

    Add ApplicationUserMovie mapping entity to enable the Watchlist feature development.

--- CinemaApp.Data/CinemaAppDbContext.cs
+++ CinemaApp.Data/CinemaAppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace CinemaApp.Data
 {
     using System.Reflection;
+
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,8 @@
         }
 
         public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        public virtual DbSet<ApplicationUserMovie> ApplicationUserMovies { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
