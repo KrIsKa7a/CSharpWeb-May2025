46560c28a575c82889cf84225b3256c352cc5d56
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-06-09 20:15:20 +0300

    Add Watchlist controller. Implement WatchlistViewModel and Index View.

---
+++ CinemaApp.Web/Controllers/WatchlistController.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using ViewModels.Watchlist;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class WatchlistController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Hardcoded empty watchlist for testing
+            IEnumerable<WatchlistViewModel> emptyWatchlist = 
+                new List<WatchlistViewModel>();
+            return View(emptyWatchlist);
+        }
+    }
+}
